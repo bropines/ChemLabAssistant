dc6775e555a32445208cff5e3b6af8625b7a23c0
bropines <bropines@gmail.com>
2026-04-03 18:49:47 +0300

    bump ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
     }
 
     // Читаем локальный файл с паролями (если он есть)
